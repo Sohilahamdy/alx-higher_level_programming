fd657cb7b095a9ed591464018613e49792525abe
Sohila hamdy <sohila.fathy22@gmail.com>
2024-07-25 08:08:50 -0700

    0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- script that deletes the database hbtn_0c_0
-DELET FROM EVEN IF NOT EXIST hbtn_0c_0;
+-- script that deletes the database hbtn_0c_0 if exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- Script Script to list all tables in a specified database
+SHOW TABLES;
