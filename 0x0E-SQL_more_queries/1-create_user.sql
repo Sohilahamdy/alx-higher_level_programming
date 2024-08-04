fb94a3f91c6978761e7edf66cbd2640a137039da
Sohila hamdy <sohila.fathy22@gmail.com>
2024-08-04 01:43:45 -0700

    0x0E. SQL - More queries

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Script to create user user_0d_1 with all privileges
+-- Check if user exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply changes
+FLUSH PRIVILEGES;
