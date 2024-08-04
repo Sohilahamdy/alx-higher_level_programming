fbbe418e944859c1a9556adcda32b16c626e72ae
Sohila hamdy <sohila.fathy22@gmail.com>
2024-08-04 03:00:01 -0700

    0x0E. SQL - More queries

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,12 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT 
+    tg.name AS genre, 
+    COUNT(tg.id) AS number_of_shows
+FROM 
+    tv_genres tg
+JOIN 
+    tv_show_genres tsg ON tg.id = tsg.genre_id
+GROUP BY 
+    tg.name
+ORDER BY 
+    number_of_shows DESC;
