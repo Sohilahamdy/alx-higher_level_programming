5e03ac4cf48b7581928ba77b7c7097a77e588c54
Sohila hamdy <sohila.fathy22@gmail.com>
2024-08-04 01:20:23 -0700

    0x0D. SQL - Introduction

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,10 @@
+-- displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
+SELECT
+    city,
+    ROUND(AVG(value), 4) AS avg_temp
+FROM
+    temperatures
+GROUP BY
+    city
+ORDER BY
+    avg_temp DESC;
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,13 @@
+-- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+SELECT
+    city,
+    ROUND(AVG(value), 4) AS avg_temp
+FROM
+    temperatures
+WHERE
+    month IN (7, 8)
+GROUP BY
+    city
+ORDER BY
+    avg_temp DESC
+LIMIT 3;
