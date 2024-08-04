68de732fdaa0a5aab38b38e5f987e35c4ffb95a4
Sohila hamdy <sohila.fathy22@gmail.com>
2024-08-04 03:32:10 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,5 @@
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT tg.name AS genre, COALESCE(SUM(tsr.rate), 0) AS rating
+SELECT tg.name AS name, COALESCE(SUM(tsr.rate), 0) AS rating
 FROM tv_genres tg
 LEFT JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
 LEFT JOIN tv_show_ratings tsr ON tsg.show_id = tsr.show_id
